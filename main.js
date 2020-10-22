71316b12fda467047e459ec1d6ecce4bdc04bcb6
Ramen Noodle <67561249+Ramenality@users.noreply.github.com>
2020-10-23 02:30:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-  
 var raha = 10
 var masin = []
 var lastUpdate = Date.now()
@@ -28,8 +27,10 @@ function OstaMasin(i) {
   g.ostetud += 1
   g. korrutaja *= 1.005
   if (g.ostetud < 20) g.korrutaja = Math.pow(g.korrutaja, 1.04) * 1.2 * Math.pow(1.2, 1.4645) - 0.522 * g.korrutaja
-  if (g.ostetud > 20) g.korrutaja *= 1.15
-  if (g.ostetud < 50) g.korrutaja *= 1.2
+  if (g.ostetud > 20) g.korrutaja *= 1.05
+  if (g.ostetud > 50) g.korrutaja *= (g.bought-45)/(0.1*g.bought)
+  if (g.ostetud > 50) g.korrutaja *= 1.05
+  if (g.ostetud < 50) g.korrutaja *= 1.15
   g.hind = Math.pow(g.hind, 1.06)
 }
 
