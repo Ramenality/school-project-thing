25a732b5d1c6593c1958a643d3292b54c57bfc39
Ramen Noodle <67561249+Ramenality@users.noreply.github.com>
2020-10-22 22:58:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function OstaMasin(i) {
   g.ostetud += 1
   g.korrutaja = Math.pow(g.korrutaja, 1.05) * 1.2 * Math.pow(1.2, 1.4845) - 0.522 * g.korrutaja
   if (g.ostetud > 19)  {
-     g.korrutaja = Math.pow(g.korrutaja, 1.05) * 1.2 * Math.pow(1.2, 1.3575) - 0.522 * g.korrutaja * ((g.ostetud + 1)/20)
+     g.korrutaja = Math.pow(g.korrutaja, 1.05) * 1.2 * ((g.korrutaja / 6.7) * 2) ^ ((g.korrutaja/10)/g.ostetud) - 0.522 * g.korrutaja * ((g.ostetud + 1)/20)
   }
   g.hind = Math.pow(g.hind, 1.06)
 }
